e6c7e5c86c431df394dc7c1cf7980646bcceadcb
Tarique Aman Aziz <taziz@redhat.com>
2020-06-06 21:03:03 +0530

    first commit

---
+++ src/test/java/net/openmosque/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package net.openmosque;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
